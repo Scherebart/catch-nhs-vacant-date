9e325e1f78b16aa030ab62de983d381ebf2a866d
Bartłomiej Krzywania <bartek.krzywania@gmail.com>
2023-03-13 14:39:14 +0100

    v 1.3

--- catch-vacant-date.sh
+++ catch-vacant-date.sh
@@ -14,9 +14,9 @@ function env() {
 }
 
 function config() {
-  RES_FOUND=100
-  RES_NETWORK_ERROR=10
-  RES_TICK=0
+  RET_FOUND=100
+  RET_NETWORK_ERROR=10
+  RET_TICK=0
 
   case $ENV in
     PROD)
@@ -41,37 +41,43 @@ function dostep() {
   --connect-timeout 15
   if [ $? -ne 0 ]; then
     echo NETWORK ERROR > "output/${timestamp}_ERROR"
-    return $RES_NETWORK_ERROR
+    return $RET_NETWORK_ERROR
   fi
 
   cat current_source.html | \
   grep '^[ ]*<span class="visuallyhidden">Nazwa szpitala albo przychodni : </span>' | \
-  sed 's/[ ]*<span class="visuallyhidden">Nazwa szpitala albo przychodni : <\/span>\(.*\)/\1/' | \
-  tee -a current_parsed
-
+  sed 's/[ ]*<span class="visuallyhidden">Nazwa szpitala albo przychodni : <\/span>\(.*\)/\1/' > \
+  current_parsed_facilities 
+ 
   cat current_source.html | \
   grep '^[ ]*<p class="result-date">' | \
   sed 's/[ ]*<p class="result-date">\([0-9]\{2\}\.[0-9]\{2\}\.[0-9]\{4\}\).*/\1/' | \
-  awk 'NR % 2 == 1' | \
-  tee -a current_parsed
+  awk 'NR % 2 == 1' > \
+  current_parsed_vacant_dates
 
-  local res=$RES_TICK
+  cat current_source.html | \
+  grep '^[ ]*<span>Pierwszy wolny termin<br />(<span class="date-info">' | \
+  sed 's/[ ]*<span>Pierwszy wolny termin<br \/>(<span class="date-info">stan na \([0-9]\{2\}\.[0-9]\{2\}\.[0-9]\{4\}\).*/\1/' > \
+  current_parsed_status_dates
 
-  if [ -f last_parsed ]; then
-    diff -q current_parsed last_parsed
+  paste -d '\n' current_parsed_facilities current_parsed_vacant_dates current_parsed_status_dates > current_parsed
+  cat current_parsed
+
+  local ret=$RET_TICK
+
+  if [ -f previous_parsed ]; then
+    diff -q current_parsed previous_parsed
     if [ $? -ne 0 ]; then 
       echo NEW DATA > "output/${timestamp}_NEW_DATA"
-      res=$RES_FOUND
-  
+      ret=$RET_FOUND
     fi
   fi
 
-  cat current_parsed > last_parsed
+  cat current_parsed > previous_parsed
   cat current_parsed > "output/${timestamp}_parsed"
 
-  return $res
+  return $ret
 }
-
 
 env "$@"
 config
@@ -80,18 +86,18 @@ while true
 do
   timestamp=$(date +"%F@%R:%S")
   dostep "$timestamp"
-  res=$?
+  ret=$?
   echo -------------------
 
-  case $res in
-    $RES_TICK)
-      echo --- TICK! ---
-      echo -------------
-      play -q -V0 mixkit-confirmation-tone-2867.wav gain -n -15
+  case $ret in
+    $RET_TICK)
+      echo --- CHECK $timestamp ---
+      echo ------------------------
+      play -q -V0 mixkit-on-or-off-light-switch-tap-2585.wav gain -n -10
 
       sleep $CHECK_INTERVAL
       ;;
-    $RES_FOUND)
+    $RET_FOUND)
       echo --- NEW DATA! ---
       echo -----------------
       for i in {0..5}
@@ -103,7 +109,7 @@ do
 
       sleep $CHECK_INTERVAL
       ;;
-    $RES_NETWORK_ERROR)
+    $RET_NETWORK_ERROR)
       echo --- NETWORK ERROR! ---
       echo ----------------------
       play -q -V0 mixkit-wrong-long-buzzer-954.wav gain -n 10
